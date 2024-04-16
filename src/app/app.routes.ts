148b38a09e7f82cb4dd98f6ace9db3c739b78b12
anwesha <anwesha010997@gmail.com>
2024-04-17 01:19:44 +0530

    adding style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',   redirectTo: 'landing', pathMatch: 'full' },
+    { path: 'landing', component: LandingComponent },
+    { path: 'home', component: HomeComponent }
+];
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatTabsModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
